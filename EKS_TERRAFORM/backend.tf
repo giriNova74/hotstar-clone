9fcb20145f13c4599d04b78787b1f54805c08ba9
giri prasad <giriprasad777sdv@gmail.com>
2025-07-08 12:59:31 +0100

    first commit

---
+++ EKS_TERRAFORM/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket = "sanji-kindgit remote add origin https://github.com/giriNova74/hotstar-clone.git
+git branch -M main
+git push -u origin main" # Replace with your actual S3 bucket name
+    key    = "EKS/terraform.tfstate"
+    region = "ap-south-1"
+  }
+}
