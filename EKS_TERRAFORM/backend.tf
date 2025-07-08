09563ac67b45a79da3021066b5b526c229ee419f
Giri Prasad <giriprasad777sdv@gmail.com>
2025-07-08 13:13:34 +0100

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,8 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "sanji-kindgit remote add origin https://github.com/giriNova74/hotstar-clone.git
-git branch -M main
-git push -u origin main" # Replace with your actual S3 bucket name
+    bucket = "sanji-kind" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
